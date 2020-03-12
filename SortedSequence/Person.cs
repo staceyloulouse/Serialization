369dedb48006e723014f5f569c9e9e6015724c7f
staceyloulouse <staceyloulouse@gmail.com>
2020-03-12 16:41:43 -0400

    Add project files.

---
+++ SortedSequence/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialization
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        List<Person> Children = new List<Person>();
+
+    }
+}
---
+++ SortedSequence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+namespace Serializations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = GetInitialData();
+            string json = ConvertToJson(data);
+
+
+        }
+
+        private static string ConvertToJson(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object GetInitialData()
+        {
+            throw new NotImplementedException();
+            var list = new List<Person>();
+            var person1 = new Person();
+            person1.Id = 1;
+
+            var mark = new Person();
+            mark.Id = 2;
+        }
+    }
+    private static void WriteToFile(string text)
+    {
+        
+    }
+
+    private static List<Person> GetInitialData()
+    {
+        
+    }
+
+    
+}
