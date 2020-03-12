43bb500890185dd263520606e28db86a8072dc01
staceyloulouse <staceyloulouse@gmail.com>
2020-03-12 17:03:21 -0400

    update to serialization

--- SortedSequence/Person.cs
+++ SortedSequence/Person.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Serialization
+namespace Serializations
 {
     class Person
     {
--- SortedSequence/Program.cs
+++ SortedSequence/Program.cs
@@ -22,24 +22,32 @@ namespace Serializations
 
         private static object GetInitialData()
         {
-            throw new NotImplementedException();
-            var list = new List<Person>();
+            //throw new NotImplementedException();
+            List<Person> list = new List<Person>();
             var person1 = new Person();
             person1.Id = 1;
 
-            var mark = new Person();
+            Person mark = new Person();
             mark.Id = 2;
+
+            list.Add(person1);
+            return list;
+        }
+        private static void WriteToFile(string text, string path)
+        {
+            var file = new FileStream(path, FileMode.Create);
+        }
+        private static void ModifyPersons(List<Person> data)
+        {
+            foreach(var person in data)
+            {
+                person.Id++;
+            }
         }
     }
-    private static void WriteToFile(string text)
-    {
-        
-    }
+    
 
-    private static List<Person> GetInitialData()
-    {
-        
-    }
+   
 
     
 }
